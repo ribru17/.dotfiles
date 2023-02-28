6acdcdaf8a5fe2c980cabb3f5c7bcfa28d9f02ba
Riley Bruins <ribru17@hotmail.com>
2023-02-27 20:13:07 -0800

    perhaps more idiomatic way of formatting on save...

--- init.lua
+++ init.lua
@@ -135,6 +135,26 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     command = 'lua vim.lsp.buf.format()'
 })
 
+--> MIGHT BE A BETTER WAY TO DO THIS
+--> TODO: LOOK INTO THIS
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- require("null-ls").setup({
+--     -- you can reuse a shared lspconfig on_attach callback here
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+--                     vim.lsp.buf.formatting_sync()
+--                 end,
+--             })
+--         end
+--     end,
+-- })
+
 require('bufferline').setup {
     options = {
         mode = "tabs",
