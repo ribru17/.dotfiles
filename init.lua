beb7cec5425e5483641818c59d0926666b6a1a36
ribru17 <ribru17@hotmail.com>
2023-02-28 11:37:09 -0800

    easier way to manage snippets

--- init.lua
+++ init.lua
@@ -336,23 +336,38 @@ lsp.preset('recommended')
 --   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 -- end
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local luasnip = require('luasnip')
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
             cmp.confirm { select = true }
         elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
+        elseif has_words_before() then
+            cmp.complete()
         else
             fallback()
         end
-    end, {
-        "i",
-        "s",
-    }),
+    end, { "i", "s", }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping(function(fallback)
         fallback()
