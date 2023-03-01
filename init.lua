aa4ec7bcebd2f487cfb235ebc6515e9eeb31ea54
ribru17 <ribru17@hotmail.com>
2023-02-28 20:43:41 -0800

    bind to go to definition in new tab

--- init.lua
+++ init.lua
@@ -376,6 +376,7 @@ lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gD", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", opts)
     -- ^^ go back with <C-o>
 end)
 
