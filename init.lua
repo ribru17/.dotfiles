72fb5c660cd55b32b2e27992638c566df54b7d51
ribru17 <ribru17@hotmail.com>
2023-02-28 19:10:51 -0800

    lorem snippet

--- init.lua
+++ init.lua
@@ -562,5 +562,13 @@ ls.add_snippets("html", {
         i(1, 'Document')
     }), {
         condition = conds.line_begin,
-    })
+    }),
+})
+
+ls.add_snippets("all", {
+    s({
+        trig = 'lorem',
+        name = 'Lorem Ipsum Sentence',
+        dscr = 'Generates a long lorem ipsum sentence.'
+    }, fmt([[Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet nec ullamcorper sit amet. Volutpat diam ut venenatis tellus in. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada. Eu feugiat pretium nibh ipsum. Convallis aenean et tortor at risus viverra. Libero volutpat sed cras ornare arcu. Pharetra vel turpis nunc eget lorem dolor sed viverra. Lacus laoreet non curabitur gravida arcu ac tortor dignissim. Ut eu sem integer vitae justo eget magna fermentum. Leo duis ut diam quam nulla porttitor massa id. Purus sit amet volutpat consequat mauris nunc congue. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Cursus risus at ultrices mi.]], {}), {}),
 })
