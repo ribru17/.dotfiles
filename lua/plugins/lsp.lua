7c1f5d6bab934b1a5fbb299a800843fec6f60318
Riley Bruins <ribru17@hotmail.com>
2023-04-13 14:02:57 -0700

    regain ability to use code action with haskell lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,19 +20,19 @@ return {
     config = function()
       local ht = require('haskell-tools')
       -- local buffer = vim.api.nvim_get_current_buf()
-      local def_opts = { noremap = true, silent = true }
+      -- local def_opts = { noremap = true, silent = true }
       ht.start_or_attach {
-        hls = {
-          on_attach = function(_, bufnr)
-            local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr })
-            -- haskell-language-server relies heavily on codeLenses,
-            -- so auto-refresh (see advanced configuration) is enabled by default
-            vim.keymap.set('n', '<leader>ca', vim.lsp.codelens.run, opts)
-            -- vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
-            -- buggy and sort of useless
-            -- vim.keymap.set('n', '<leader>ha', ht.lsp.buf_eval_all, opts)
-          end,
-        },
+        -- hls = {
+        --   on_attach = function(_, bufnr)
+        --     local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr })
+        --     -- haskell-language-server relies heavily on codeLenses,
+        --     -- so auto-refresh (see advanced configuration) is enabled by default
+        --     -- vim.keymap.set('n', '<leader>ca', vim.lsp.codelens.run, opts)
+        --     -- vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
+        --     -- buggy and sort of useless
+        --     -- vim.keymap.set('n', '<leader>ha', ht.lsp.buf_eval_all, opts)
+        --   end,
+        -- },
       }
     end,
   },
