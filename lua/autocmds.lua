0f9d12ddb403c84aa19fc321ac78cdf993deab3f
Riley Bruins <ribru17@hotmail.com>
2023-04-17 14:35:04 -0700

    change snippet jump clearing function (still doesn't work properly with auto snippets)

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -73,19 +73,16 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end,
 })
 
-local function leave_snippet()
-  if
-      ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
-      and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
-      and not require('luasnip').session.jump_active
-  then
-    require('luasnip').unlink_current()
-  end
-end
-
 -- prevent weird snippet jumping behavior
 -- https://github.com/L3MON4D3/LuaSnip/issues/258
 vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = '*',
-  callback = leave_snippet,
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end,
 })
