bb3e5018789d81a91f553cf31de4936309d7499a
ribru17 <ribru17@hotmail.com>
2023-04-18 17:09:02 -0700

    disable ez semicolon for markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,3 +3,6 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 
 vim.cmd [[silent exe "normal zR"]]
+
+-- disable EZ-Semicolon for markdown files
+vim.keymap.set('i', ';', ';', { remap = false, buffer = 0 })
