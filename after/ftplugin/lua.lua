15d0bee0066d17b3fc3ce9b25926ba9aaea5ad6a
Riley Bruins <ribru17@hotmail.com>
2023-05-08 10:22:00 -0700

    vastly improving lua function surround

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,7 +1,19 @@
 require('nvim-surround').buffer_setup {
   surrounds = {
     F = {
-      add = { 'function() ', ' end' },
+      add = function()
+        local result = require('nvim-surround.config').get_input(
+          'Enter the function name: ')
+        if result then
+          result = result == '' and result or ' ' .. result
+          return {
+            { 'function' .. result .. '() ' },
+            { ' end' },
+          }
+        end
+      end,
+      find = 'function.-%(%) .- end',
+      delete = '^(function.-%(%) )().-( end)()$',
     },
   },
 }
