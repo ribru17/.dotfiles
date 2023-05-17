dfd1becee21275c33ac97011a32dde90c55fa360
Riley Bruins <ribru17@hotmail.com>
2023-05-17 11:45:38 -0700

    improving fold experience

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,6 +2,3 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.foldexpr = 'NestedMarkdownFolds()'
-
--- start with all folds open
-vim.cmd [[silent exe "normal zR"]]
