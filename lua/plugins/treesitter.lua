1c2a6bf785da406948294933cc096bbd46073659
Riley Bruins <ribru17@hotmail.com>
2023-05-24 14:34:01 -0700

    add mermaid to list of parsers for markdown flowcharts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,8 @@ return {
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the first five parsers should always be installed)
         ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript',
-          'typescript', 'rust', 'tsx', 'markdown', 'markdown_inline', 'latex', },
+          'typescript', 'rust', 'tsx', 'markdown', 'markdown_inline', 'latex',
+          'mermaid', },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
