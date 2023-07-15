79db4d3b335ed473308aa8e680c1aaf3d94c738f
Riley Bruins <ribru17@hotmail.com>
2023-07-14 19:38:39 -0700

    set markdown folding options AFTER bufenter (late) because apparently that is the only way it will work now

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,10 +1,16 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
-vim.opt_local.foldexpr = 'NestedMarkdownFolds()'
 
--- start with all folds open
-vim.cmd [[silent exe "normal zR"]]
+-- vim.api.nvim_create_autocmd('BufEnter', {
+--   once = true,
+--   callback = function()
+--     vim.opt_local.foldexpr = 'NestedMarkdownFolds()'
+
+--     -- start with all folds open
+--     vim.cmd [[silent exe "normal zR"]]
+--   end,
+-- })
 
 require('nvim-surround').buffer_setup {
   aliases = {
