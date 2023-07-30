1e634bd3749c9cef2c641d776e23b50a1e107c98
Riley Bruins <ribru17@hotmail.com>
2023-07-29 17:39:53 -0700

    autotag doc comment

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -6,6 +6,8 @@ return {
       'jsx', 'rescript', 'php', 'glimmer', 'handlebars', 'hbs',
       'markdown',
     },
+    -- disabled here because I have it overridden somewhere else in order to
+    -- achieve compatibility with luasnip
     opts = { enable_close_on_slash = false },
   },
   {
