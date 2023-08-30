39fcf0b24be23388fa6dd4084ab3cb8a4baf3078
Riley Bruins <ribru17@hotmail.com>
2023-08-29 21:11:47 -0700

    improve javascript family files arrow function surround to delete const variable creation, if any

--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -1,3 +1,4 @@
+local config = require('nvim-surround.config')
 require('nvim-surround').buffer_setup {
   surrounds = {
     F = {
@@ -12,8 +13,30 @@ require('nvim-surround').buffer_setup {
           { '}' },
         }
       end,
-      find = '%(%) => {.-}',
-      delete = '^(%(%) => {)().-(})()$',
+      find = function()
+        local named_match = config.get_selection('const [%w_]- = %(%) => {.-}')
+        if named_match then
+          return named_match
+        else
+          return config.get_selection('%(%) => {.-}')
+        end
+      end,
+      delete = function()
+        local named_match = config.get_selection {
+          pattern = 'const [%w_]- = %(%) => {',
+        }
+        if not named_match then
+          named_match = config.get_selection {
+            pattern = '%(%) => {',
+          }
+        end
+        return {
+          left = named_match,
+          right = config.get_selection {
+            pattern = '}',
+          },
+        }
+      end,
     },
   },
 }
