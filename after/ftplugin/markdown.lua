c9ab3dfee98a56bae02c20eb5abdd7a132e18ea3
Riley Bruins <ribru17@hotmail.com>
2023-09-22 22:10:19 -0700

    allowing bullets.vim and nvim autopairs to coexist

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = 'nc'
--- Allow block comments to be continued when hitting enter
+-- Allow block comments to be continued when hitting enter.
 vim.opt_local.formatoptions:append('qcro')
 
 -- NestedMarkdownFolds() doesn't refresh against telescope folding bug
@@ -14,6 +14,20 @@ vim.api.nvim_create_autocmd('BufEnter', {
     vim.cmd.normal('zx')
   end,
 })
+
+-- Allow bullets.vim and nvim-autopairs to coexist.
+vim.schedule(function()
+  vim.keymap.set('i', '<CR>', function()
+    local pair = require('nvim-autopairs').completion_confirm()
+    if pair == vim.api.nvim_replace_termcodes('<CR>', true, false, true) then
+      vim.cmd.InsertNewBullet()
+    else
+      vim.cmd.normal { 'i' .. pair, bang = true }
+    end
+  end, {
+    buffer = 0,
+  })
+end)
 
 require('nvim-surround').buffer_setup {
   aliases = {
