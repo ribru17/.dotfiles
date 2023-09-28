d45128f58cfdb2dfad0587b64d4bc52ec75eef04
Riley Bruins <ribru17@hotmail.com>
2023-09-27 18:16:41 -0700

    better tabbing of selections

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -11,8 +11,12 @@ of both worlds).
 })
 
 local indent_opts = { desc = 'VSCode-style block indentation' }
-map('x', '<Tab>', '>gv', indent_opts)
-map('x', '<S-Tab>', '<gv', indent_opts)
+map('x', '<Tab>', function()
+  vim.cmd.normal(vim.v.count1 .. '>gv')
+end, indent_opts)
+map('x', '<S-Tab>', function()
+  vim.cmd.normal(vim.v.count1 .. '<gv')
+end, indent_opts)
 map('n', '<Tab>', '>>', indent_opts)
 map('n', '<S-Tab>', '<<', indent_opts)
 
