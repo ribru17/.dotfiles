bf8e4a8de79e1b0141120b8a8348dd487de4ce27
Riley Bruins <ribru17@hotmail.com>
2023-09-28 19:51:18 -0700

    specify markdown preview link weirdness

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,8 @@
 return {
   {
     'ribru17/markdown-preview.nvim',
+    -- anchor links have an issue, see
+    -- https://github.com/iamcco/markdown-preview.nvim/pull/575
     ft = 'markdown',
     build = function()
       vim.fn['mkdp#util#install']()
