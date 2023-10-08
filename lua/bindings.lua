77a6b71aab1915252496c60c7cdc4045367709b7
Riley Bruins <ribru17@hotmail.com>
2023-10-08 09:58:13 -0700

    command to format only diff changes

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -346,4 +346,33 @@ create_command(
   { desc = 'Easier Markdown preview alias' }
 )
 
+create_command('DiffFormat', function()
+  local lines = vim.fn.system('git diff --unified=0'):gmatch('[^\n\r]+')
+  local ranges = {}
+  for line in lines do
+    if line:find('^@@') then
+      local line_nums = line:match('%+.- ')
+      if line_nums:find(',') then
+        local _, _, first, second = line_nums:find('(%d+),(%d+)')
+        table.insert(ranges, {
+          start = { tonumber(first), 0 },
+          ['end'] = { tonumber(first) + tonumber(second), 0 },
+        })
+      else
+        local first = tonumber(line_nums:match('%d+'))
+        table.insert(ranges, {
+          start = { first, 0 },
+          ['end'] = { first + 1, 0 },
+        })
+      end
+    end
+  end
+  local format = require('conform').format
+  for _, range in pairs(ranges) do
+    format {
+      range = range,
+    }
+  end
+end, { desc = 'Format changed lines' })
+
 --> END OF MISCELLANEOUS USER COMMANDS <--
