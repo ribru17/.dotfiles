0691ebc9043ba2f131b33a641dc09f6ccf377f11
Riley Bruins <ribru17@hotmail.com>
2023-10-09 10:57:09 -0700

    remove redundant tabstop settings for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,3 @@
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = 'nc'
 vim.opt_local.foldcolumn = '0'
