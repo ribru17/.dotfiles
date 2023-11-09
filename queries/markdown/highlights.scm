50351fe95218b8711279cf5f3f4e8713e576d104
Riley Bruins <ribru17@hotmail.com>
2023-11-09 11:12:08 -0800

    make markdown conceals more readable

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -4,56 +4,73 @@
 ; impossible because both nodes are handled by different parsers)
 
 ; Bullet points
-([(list_marker_minus) (list_marker_star)] @punctuation.special (#offset! @punctuation.special 0 0 0 -1) (#set! conceal "•"))
+([(list_marker_minus) (list_marker_star)]
+ @punctuation.special
+ (#offset! @punctuation.special 0 0 0 -1)
+ (#set! conceal "•"))
 (list
- (list_item
-   (list
-     (list_item
-       ([(list_marker_minus) (list_marker_star)] @punctuation.special (#offset! @punctuation.special 0 0 0 -1) (#set! conceal "⭘"))
-       ))))
+  (list_item
+    (list
+      (list_item
+        ([(list_marker_minus) (list_marker_star)]
+         @punctuation.special
+         (#offset! @punctuation.special 0 0 0 -1)
+         (#set! conceal "⭘"))))))
 (list
- (list_item
-   (list
-     (list_item
-       (list
-         (list_item
-           ([(list_marker_minus) (list_marker_star)] @punctuation.special (#offset! @punctuation.special 0 0 0 -1) (#set! conceal "◼"))
-           ))))))
+  (list_item
+    (list
+      (list_item
+        (list
+          (list_item
+            ([(list_marker_minus) (list_marker_star)]
+             @punctuation.special
+             (#offset! @punctuation.special 0 0 0 -1)
+             (#set! conceal "◼"))))))))
 (list
- (list_item
-  (list
-    (list_item
-      (list
-        (list_item
-          (list
-            (list_item
-              ([(list_marker_minus) (list_marker_star)] @punctuation.special (#offset! @punctuation.special 0 0 0 -1) (#set! conceal "◻"))
-              ))))))))
+  (list_item
+    (list
+      (list_item
+        (list
+          (list_item
+            (list
+              (list_item
+                ([(list_marker_minus) (list_marker_star)]
+                 @punctuation.special
+                 (#offset! @punctuation.special 0 0 0 -1)
+                 (#set! conceal "◻"))))))))))
 (list
- (list_item
-  (list
-    (list_item
-      (list
-        (list_item
-          (list
-            (list_item
-              (list
-                (list_item
-                  ([(list_marker_minus) (list_marker_star)] @punctuation.special (#offset! @punctuation.special 0 0 0 -1) (#set! conceal "→"))
-                  ))))))))))
+  (list_item
+    (list
+      (list_item
+        (list
+          (list_item
+            (list
+              (list_item
+                (list
+                  (list_item
+                    ([(list_marker_minus) (list_marker_star)]
+                     @punctuation.special
+                     (#offset! @punctuation.special 0 0 0 -1)
+                     (#set! conceal "→"))))))))))))
 
 ; Checkbox list items
-((task_list_marker_unchecked) @text.todo.unchecked (#offset! @text.todo.unchecked 0 -2 0 0) (#set! conceal "✗"))
-((task_list_marker_checked) @text.todo.checked (#offset! @text.todo.checked 0 -2 0 0) (#set! conceal "✔"))
+((task_list_marker_unchecked)
+ @text.todo.unchecked
+ (#offset! @text.todo.unchecked 0 -2 0 0)
+ (#set! conceal "✗"))
+((task_list_marker_checked)
+ @text.todo.checked
+ (#offset! @text.todo.checked 0 -2 0 0)
+ (#set! conceal "✔"))
 (list_item (task_list_marker_checked) (_) @comment)
 
 ; Tables
-(pipe_table_header ("|") @punctuation.special @conceal (#set! conceal "┃"))
-(pipe_table_delimiter_row ("|") @punctuation.special @conceal (#set! conceal "┃"))
-(pipe_table_delimiter_cell ("-") @punctuation.special @conceal (#set! conceal "━"))
-((pipe_table_align_left) @punctuation.special @conceal (#set! conceal "┣"))
-((pipe_table_align_right) @punctuation.special @conceal (#set! conceal "┫"))
-(pipe_table_row ("|") @punctuation.special @conceal (#set! conceal "┃"))
+(pipe_table_header ("|") @punctuation.special (#set! conceal "┃"))
+(pipe_table_delimiter_row ("|") @punctuation.special (#set! conceal "┃"))
+(pipe_table_delimiter_cell ("-") @punctuation.special (#set! conceal "━"))
+((pipe_table_align_left) @punctuation.special (#set! conceal "┣"))
+((pipe_table_align_right) @punctuation.special (#set! conceal "┫"))
+(pipe_table_row ("|") @punctuation.special (#set! conceal "┃"))
 
 ; Block quotes
 ((block_quote_marker) @punctuation.special
