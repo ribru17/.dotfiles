b500fa546e6eba8286152c42bb897b1be1bcd102
Riley Bruins <ribru17@hotmail.com>
2023-11-12 15:19:20 -0800

    replace empty checkbox with heavy x

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -57,7 +57,7 @@
 ((task_list_marker_unchecked)
  @text.todo.unchecked
  (#offset! @text.todo.unchecked 0 -2 0 0)
- (#set! conceal "✗"))
+ (#set! conceal "✘"))
 ((task_list_marker_checked)
  @text.todo.checked
  (#offset! @text.todo.checked 0 -2 0 0)
