086fc6dbaedeea6d557a49ca03b03ebf489f34bd
Riley Bruins <ribru17@hotmail.com>
2023-11-27 16:38:42 -0800

    comment clarification

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -1,9 +1,9 @@
 ;; extends
 
 ; NOTE: It would be nice to prevent conceals in table cells (because they can
-; mess up pipe alignment) but this is currently impossible since base conceals
-; are handled internally, and there is no way to detect if other inline conceals
-; are within table cells because detection is split among the parsers.
+; mess up pipe alignment) but this is currently impossible since there is no way
+; to detect if other inline conceals are within table cells because detection is
+; split among the parsers.
 
 ; Bullet points
 ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
