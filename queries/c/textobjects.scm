ede64a0772fbbc9ce487d4a89eec40fbd96980b8
Riley Bruins <ribru17@hotmail.com>
2023-12-13 10:23:29 -0800

    change sizeof queries due to parser bugs

--- queries/c/textobjects.scm
+++ queries/c/textobjects.scm
@@ -2,12 +2,9 @@
 
 ; Text object `call` for `sizeof` expressions
 
-(sizeof_expression) @call.outer
-
 (sizeof_expression
  value: (parenthesized_expression . "(" . (_) @_start (_)? @_end . ")"
- (#make-range! "call.inner" @_start @_end)))
+ (#make-range! "call.inner" @_start @_end))) @call.outer
 
 (sizeof_expression
- type: (type_descriptor) @call.inner
- )
+ type: (type_descriptor) @call.inner) @call.outer
