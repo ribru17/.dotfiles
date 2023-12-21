6ed9eb62d9ededa15af20f2da02305080ec79261
Riley Bruins <ribru17@hotmail.com>
2023-12-20 19:15:20 -0800

    Workaround for unfortunate Rust parser macro issue

---
+++ queries/rust/highlights.scm
@@ -0,0 +1,4 @@
+;; extends
+
+; Fixes an issue where function calls aren't properly recognized within a macro
+(macro_invocation (token_tree (identifier) @function.call . (token_tree)))
--- queries/rust/textobjects.scm
+++ queries/rust/textobjects.scm
@@ -2,7 +2,14 @@
 
 ; TODO: Remove upon merging of the following PR:
 ; https://github.com/nvim-treesitter/nvim-treesitter-textobjects/pull/535
-
 (macro_invocation) @call.outer
 (macro_invocation (token_tree . "(" . (_) @_start (_)? @_end . ")"
   (#make-range! "call.inner" @_start @_end)))
+
+; Fixes an issue where function calls aren't properly recognized within a macro
+(macro_invocation (token_tree
+ (identifier) @_start .
+ (token_tree . "(" .
+  (_) @_innerstart (_)? @_innerend . ")") @_end)
+   (#make-range! "call.inner" @_innerstart @_innerend)
+   (#make-range! "call.outer" @_start @_end))
