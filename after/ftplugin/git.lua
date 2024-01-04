0658c39f876e048494beeb7c3716c27d3cd65ea7
Riley Bruins <ribru17@hotmail.com>
2024-01-04 00:53:03 -0800

    Fire TermOpen automatically for Fugitive terminals
    I think this should be happening automatically. This is either a bug or
    an artifact of Vim's `autocmd` ordering.

--- after/ftplugin/git.lua
+++ after/ftplugin/git.lua
@@ -1,3 +1,5 @@
 -- use delta when showing e.g. Fugitive diffs
 vim.cmd.term { 'cat', '%', '|', 'delta', '--paging=always' }
 vim.cmd.startinsert()
+-- not sure why this doesn't happen automatically...
+vim.cmd.doautocmd('TermOpen')
