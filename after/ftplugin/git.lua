550dc8e0c34c186d8399ec7264e20604a88a4456
Riley Bruins <ribru17@hotmail.com>
2024-01-05 09:33:10 -0800

    Removing hacky Fugitive-Delta integration
    This is now handled in the .gitconfig itself. See:
    https://www.reddit.com/r/neovim/comments/18yi3u5/comment/kggcgsv/?utm_source=share&utm_medium=web2x&context=3

--- after/ftplugin/git.lua
+++
@@ -1,7 +0,0 @@
-if vim.fn.executable('delta') == 1 then
-  -- use delta when showing e.g. Fugitive diffs
-  vim.cmd.term { 'cat', '%', '|', 'delta', '--paging=always' }
-  vim.cmd.startinsert()
-  -- not sure why this doesn't happen automatically...
-  vim.cmd.doautocmd('TermOpen')
-end
