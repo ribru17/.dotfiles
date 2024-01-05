efe2a08697ae4591991f712968e6df35bff24cad
Riley Bruins <ribru17@hotmail.com>
2024-01-04 20:21:43 -0800

    Test commit on test branch

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy plugin manager
+-- bootstrap lazy plugin manager
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
