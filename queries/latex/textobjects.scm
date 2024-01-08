7c19d05cb0559b4211da19397c700aa356107853
Riley Bruins <ribru17@hotmail.com>
2024-01-08 12:44:05 -0800

    Slight latex math object improvements

--- queries/latex/textobjects.scm
+++ queries/latex/textobjects.scm
@@ -1,6 +1,6 @@
 ;; extends
 
-((inline_formula) @math.inner (#offset! @math.inner 0 1 0 -1))
-((displayed_equation) @math.inner (#offset! @math.inner 0 2 0 -2))
-((inline_formula) @math.outer)
-((displayed_equation) @math.outer)
+((inline_formula . "$" . (_) @_start (_)? @_end . "$" .) @math.outer
+  (#make-range! "math.inner" @_start @_end))
+((displayed_equation . "$$" . (_) @_start (_)? @_end . "$$" .) @math.outer
+  (#make-range! "math.inner" @_start @_end))
