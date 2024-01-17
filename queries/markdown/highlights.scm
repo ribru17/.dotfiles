df4f54f6c4a9da718ac4862131f3e3881fb9b95f
Riley Bruins <ribru17@hotmail.com>
2024-01-17 09:04:47 -0800

    Testing different checkbox conceals
    Also adding more alternate symbols

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -75,11 +75,11 @@
 ; Checkbox list items
 ((task_list_marker_unchecked) @text.todo.unchecked
   (#offset! @text.todo.unchecked 0 -2 0 0)
-  (#set! conceal "✗")) ; Alt: ✘
+  (#set! conceal "☐")) ; Alts:✗ ✘ 🗷 ❌☐
 
 ((task_list_marker_checked) @text.todo.checked
   (#offset! @text.todo.checked 0 -2 0 0)
-  (#set! conceal "✓")) ; Alt: ✔
+  (#set! conceal "☑")) ; Alts:✓ ✔ 🗹 ✅☑
 
 (list_item
   (task_list_marker_checked)
