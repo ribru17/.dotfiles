10053132cc1bbc0990f2d8b6a89676917701def7
Riley Bruins <ribru17@hotmail.com>
2024-01-18 14:35:59 -0800

    Easier help tag navigation

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -2,4 +2,9 @@
 vim.cmd.wincmd('T')
 -- get highlighted code examples
 vim.treesitter.start()
+-- easier navigation
 vim.opt_local.relativenumber = true
+-- better mnemonic for tag jumping
+vim.keymap.set('n', 'gd', '<C-]>', {
+  buffer = true,
+})
