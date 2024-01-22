d8dcab0b8ed4bdeb8763a64da87028100052e90a
Riley Bruins <ribru17@hotmail.com>
2024-01-21 16:53:12 -0800

    Swag injections for Git Bash command aliases

---
+++ queries/git_config/injections.scm
@@ -0,0 +1,25 @@
+; extends
+; Alias bash injections
+; TODO: Make a PR for this
+(section
+  (section_header
+    (section_name) @_section)
+  (variable
+    value: (string) @injection.content)
+  (#eq? @_section "alias")
+  (#lua-match? @injection.content "^!")
+  (#offset! @injection.content 0 1 0 0)
+  (#set! injection.language "bash"))
+
+(section
+  (section_header
+    (section_name) @_section)
+  (variable
+    value:
+      (string
+        "\""
+        "\"") @injection.content)
+  (#eq? @_section "alias")
+  (#lua-match? @injection.content "^\"!")
+  (#offset! @injection.content 0 2 0 -1)
+  (#set! injection.language "bash"))
