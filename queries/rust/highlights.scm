8e5b8731be42317de187b2d71e7e4cc8c9dc8007
Riley Bruins <ribru17@hotmail.com>
2024-01-23 22:47:39 -0800

    Remove hacky Rust queries: Fixed on master!

--- queries/rust/highlights.scm
+++
@@ -1,7 +0,0 @@
-; extends
-; Fixes an issue where function calls aren't properly recognized within a macro
-(macro_invocation
-  (token_tree
-    (identifier) @function.call
-    .
-    (token_tree)))
