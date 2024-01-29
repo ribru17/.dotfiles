4738a7152c28bf221f8a55ada0259d2ed2dffa0d
Riley Bruins <ribru17@hotmail.com>
2024-01-28 21:26:25 -0800

    Installing yay along with dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Installs system dependencies.
-sudo pacman -S neovim curl gcc nodejs python3 kitty git-delta ripgrep wl-clipboard unzip less bat
+echo "Updating system:"
+sudo pacman -Syu
+echo "Installing dependencies:"
+sudo pacman -S --needed neovim nodejs python3 kitty git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
+echo "Installing Yay:"
+cd
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
