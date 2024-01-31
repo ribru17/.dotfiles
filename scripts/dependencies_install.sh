2bfc751347e4795173a288a7fca101af88971cf2
Riley Bruins <ribru17@hotmail.com>
2024-01-31 08:57:29 -0800

    Fastfetch now in main Arch package db

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -4,9 +4,8 @@
 echo "Updating system:"
 sudo pacman -Syu
 echo "Installing dependencies:"
-sudo pacman -S --needed neovim nodejs npm python3 kitty git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
+sudo pacman -S --needed fastfetch neovim nodejs npm python3 kitty git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
 echo "Installing Yay:"
 cd && git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
-yay -S --needed fastfetch
