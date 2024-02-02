ba48bbb94c642b6a5e7058151acf159a306ca301
Riley Bruins <ribru17@hotmail.com>
2024-02-01 17:20:17 -0800

    Adding `blesh` and git to dependencies list

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -4,8 +4,11 @@
 echo "Updating system:"
 sudo pacman -Syu
 echo "Installing dependencies:"
-sudo pacman -S --needed fastfetch neovim nodejs npm python3 kitty git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
+sudo pacman -S --needed fastfetch neovim nodejs npm python3 kitty git git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
 echo "Installing Yay:"
 cd && git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
+echo "Installing blesh:"
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
