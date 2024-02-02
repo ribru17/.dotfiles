f86c9eb3f05b7aedff90858ea549c72c47ea235e
Riley Bruins <ribru17@hotmail.com>
2024-02-01 17:38:35 -0800

    Better blesh installation script

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -4,11 +4,14 @@
 echo "Updating system:"
 sudo pacman -Syu
 echo "Installing dependencies:"
-sudo pacman -S --needed fastfetch neovim nodejs npm python3 kitty git git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less
+sudo pacman -S --needed fastfetch neovim nodejs npm python3 kitty git git-delta ripgrep wl-clipboard bat reflector base-devel curl gcc unzip less xz
 echo "Installing Yay:"
 cd && git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
 echo "Installing blesh:"
-git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=~/.local
+wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
+mkdir -p ~/.local/share/blesh
+cp -Rf ble-nightly/* ~/.local/share/blesh/
+rm -rf ble-nightly
+source ~/.local/share/blesh/ble.sh
