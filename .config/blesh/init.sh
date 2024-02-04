641d4e44e6e8443910c3e743f6cdd5144dd2373a
Riley Bruins <ribru17@hotmail.com>
2024-02-03 20:36:32 -0800

    Discard line with ctrl+c in Bash Vim mode

--- .config/blesh/init.sh
+++ .config/blesh/init.sh
@@ -15,6 +15,7 @@ ble-bind -f 'C-SP' 'complete show_menu'
 ble-bind -m auto_complete -f 'C-e' auto_complete/cancel
 ble-bind -m isearch -f 'RET' isearch/accept-line
 ble-bind -m isearch -f 'C-m' isearch/accept-line
+ble-bind -m vi_imap -f 'C-c' discard-line
 # for kitty
 ble-bind -m auto_complete -f C-i auto_complete/insert
 ble-bind -f 'M-DEL' kill-backward-fword
