793cf49129a03955fa5c43fcd8b502cfc80a4ab1
Riley Bruins <ribru17@hotmail.com>
2024-02-03 20:51:40 -0800

    Option to install TLP as a dependency

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -24,6 +24,7 @@ sudo pacman -S --needed\
     less\
     xz\
     bash-completion
+
 echo "Installing Yay:"
 cd && git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
@@ -39,3 +40,12 @@ tempfile=$(mktemp) \
     && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
     && tic -x -o ~/.terminfo $tempfile \
     && rm $tempfile
+
+read -p "Are you on a laptop? (If yes, TLP will be installed to preserve battery life. Do NOT accept if you are on a desktop.) [y/N] " ans
+if [ "$ans" = "y" ] || [ "$ans" = "Y" ]; then
+    sudo pacman -S --needed tlp ethtool
+    sudo systemctl enable tlp
+    sudo systemctl start tlp
+    sudo systemctl mask systemd-rfkill.service
+    sudo systemctl mask systemd-rfkill.socket
+fi
