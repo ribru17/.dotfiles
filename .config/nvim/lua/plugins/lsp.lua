fc9a7d45a8b454ebe77195c3a0e8370b101cc8c9
Riley Bruins <ribru17@hotmail.com>
2024-02-06 13:22:39 -0800

    Option to remove TS suggestions

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -443,7 +443,15 @@ return {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
-    opts = {},
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          -- NOTE: Should only really enable this if working in a Tree-sitter
+          -- grammar
+          disableSuggestions = true,
+        },
+      },
+    },
   },
   {
     'aznhe21/actions-preview.nvim',
