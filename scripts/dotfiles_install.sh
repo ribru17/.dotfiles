58f1a1602cd04c81469b3775a4421eaa5cd7745f
Riley Bruins <ribru17@hotmail.com>
2024-02-15 10:47:52 -0800

    Fix some issues with dotfile installation script

--- scripts/dotfiles_install.sh
+++ scripts/dotfiles_install.sh
@@ -6,17 +6,19 @@ if ! command -v git &>/dev/null; then
     sudo pacman -S git
 fi
 # clone and install dotfiles
-git clone git@github.com:ribru17/.dotfiles.git "$HOME/.dotfiles"
-alias dots='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dots config --local status.showUntrackedFiles no
+git clone --bare git@github.com:ribru17/.dotfiles.git "$HOME/.dotfiles"
+function dots {
+    git "--git-dir=$HOME/.dotfiles/" "--work-tree=$HOME" "$@"
+}
+dots config status.showUntrackedFiles no
 # so fugitive can recognize the dotfiles working tree
 # see: https://stackoverflow.com/a/66624354
-dots config --local core.worktree "$HOME"
+dots config core.worktree "$HOME"
 if ! dots checkout; then
-    echo "Installed config!"
-else
     echo "Must move or delete conflicting files."
     exit
+else
+    echo "Installed config!"
 fi
 # optionally (preferably) install some system dependencies
 read -rp "Install all package dependencies? [Y/n] " ans
