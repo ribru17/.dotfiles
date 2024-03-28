7e1a3a48a6ee4aac6a83a149e771763fe954d7fc
Riley Bruins <ribru17@hotmail.com>
2024-03-28 10:55:46 -0700

    Add home-manager

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -1,14 +1,25 @@
 {
   description = "NixOS configuration with flakes";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  inputs = {
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware, home-manager }: {
     nixosConfigurations.frametop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
         nixos-hardware.nixosModules.framework-13-7040-amd
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rileyb = import ./home.nix;
+        }
       ];
     };
   };
