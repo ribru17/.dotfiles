2f18d1ab7f67abc72af2d066b3b91fe2f3bc7b9b
Riley Bruins <ribru17@hotmail.com>
2024-09-08 19:02:44 -0700

    Markdown injections for gitcommit content

--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,4 @@
 vim.wo.spell = true
 vim.wo.colorcolumn = '50'
+vim.wo.conceallevel = 2
+vim.wo.concealcursor = 'nc'
--- .config/nvim/queries/gitcommit/injections.scm
+++ .config/nvim/queries/gitcommit/injections.scm
@@ -2,3 +2,10 @@
 
 ((subject) @injection.content
   (#set! injection.language "markdown_inline"))
+
+(source
+  (subject)
+  .
+  (message) @injection.content
+  (#set! injection.language "markdown")
+  (#set! injection.include-children))
