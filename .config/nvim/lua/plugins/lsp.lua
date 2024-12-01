0c3435644951ae0abbba2e08fd236070edfac664
Riley Bruins <ribru17@hotmail.com>
2024-12-01 09:34:17 -0800

    Go inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -122,6 +122,15 @@ return {
               unusedparams = true,
             },
             staticcheck = true,
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
           },
         },
       }
