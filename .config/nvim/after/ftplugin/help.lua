cd841f482d042999158747985b658f39c7d5d927
Riley Bruins <ribru17@hotmail.com>
2024-12-10 09:44:26 -0800

    No sign column in help buffers

--- .config/nvim/after/ftplugin/help.lua
+++ .config/nvim/after/ftplugin/help.lua
@@ -1,6 +1,7 @@
 -- easier navigation
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.wo.signcolumn = 'no'
 -- better mnemonic for tag jumping
 vim.keymap.set({ 'n', 'x' }, 'gd', '<C-]>', {
   buffer = true,
