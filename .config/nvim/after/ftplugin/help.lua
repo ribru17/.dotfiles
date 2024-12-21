44b4de79e6d9ed0f4cc72bd66699a20bf5b95713
Riley Bruins <ribru17@hotmail.com>
2024-12-20 18:50:44 -0800

    Allow signcolumn in help files again

--- .config/nvim/after/ftplugin/help.lua
+++ .config/nvim/after/ftplugin/help.lua
@@ -1,7 +1,6 @@
 -- easier navigation
 vim.wo.relativenumber = true
 vim.wo.number = true
-vim.wo.signcolumn = 'no'
 -- better mnemonic for tag jumping
 vim.keymap.set({ 'n', 'x' }, 'gd', '<C-]>', {
   buffer = true,
