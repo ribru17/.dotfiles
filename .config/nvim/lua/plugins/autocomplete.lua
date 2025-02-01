89bb3dfd589d5d309d1f3a131bb52045f99fb424
Riley Bruins <ribru17@hotmail.com>
2025-02-01 08:43:48 -0800

    Update spell enable function

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -219,12 +219,17 @@ return {
                 -- Only enable source in `@spell` captures, and disable it in
                 -- `@nospell` captures
                 enable_in_context = function()
-                  local captures = vim.treesitter.get_captures_at_cursor(0)
+                  local curpos = vim.api.nvim_win_get_cursor(0)
+                  local captures = vim.treesitter.get_captures_at_pos(
+                    0,
+                    curpos[1] - 1,
+                    curpos[2] - 1
+                  )
                   local in_spell_capture = false
-                  for _, capture in ipairs(captures) do
-                    if capture == 'spell' then
+                  for _, cap in ipairs(captures) do
+                    if cap.capture == 'spell' then
                       in_spell_capture = true
-                    elseif capture == 'nospell' then
+                    elseif cap.capture == 'nospell' then
                       return false
                     end
                   end
