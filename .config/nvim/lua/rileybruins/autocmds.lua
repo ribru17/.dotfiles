f369edea5159188a0d7e2fdc4742e1e92f80a510
Riley Bruins <ribru17@hotmail.com>
2025-02-10 22:00:14 -0800

    Override query omnifunc with LSP one

--- .config/nvim/lua/rileybruins/autocmds.lua
+++ .config/nvim/lua/rileybruins/autocmds.lua
@@ -290,6 +290,10 @@ create_autocmd('FileType', {
         ),
       },
       root_dir = vim.fs.root(0, { 'queries' }),
+      -- OPTIONAL: Override the query omnifunc
+      on_attach = function(_, buf)
+        vim.bo[buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      end,
       init_options = {
         parser_install_directories = {
           -- If using nvim-treesitter with lazy.nvim
