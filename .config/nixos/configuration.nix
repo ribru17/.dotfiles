8f03f2d71fa1d3d7a7540758452bc2157f053834
Riley Bruins <ribru17@hotmail.com>
2025-05-04 07:44:13 -0700

    Migrate package version (chore)

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -155,7 +155,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  nix.package = pkgs.nixVersions.git;
+  nix.package = pkgs.nixVersions.latest;
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.settings.experimental-features = [
     "nix-command"
