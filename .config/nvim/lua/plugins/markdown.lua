f55493e8bfcc4d1a472ac38a4c9ea6d921098ddd
Riley Bruins <ribru17@hotmail.com>
2025-12-13 10:30:52 -0800

    Switch maintained markdown previewer plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,7 @@
 return {
   {
-    'ribru17/markdown-preview.nvim',
-    -- anchor links have an issue, see
-    -- https://github.com/iamcco/markdown-preview.nvim/pull/575
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = 'markdown',
-    build = 'cd app && npx --yes yarn install',
+    'brianhuster/live-preview.nvim',
+    cmd = 'LivePreview',
   },
   {
     'dkarter/bullets.vim',
